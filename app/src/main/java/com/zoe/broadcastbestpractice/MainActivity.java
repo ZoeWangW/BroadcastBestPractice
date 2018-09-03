af25009710c6b52ad5f94c273c0622a37fff132d
ZoeWangW <13438165026@163.com>
2018-09-03 20:24:53 +0800

    实践登录并广播强制线下（广播强制下线功能未实现）
    Signed-off-by: ZoeWangW <13438165026@163.com>

--- app/src/main/java/com/zoe/broadcastbestpractice/MainActivity.java
+++ app/src/main/java/com/zoe/broadcastbestpractice/MainActivity.java
@@ -2,16 +2,10 @@ package com.zoe.broadcastbestpractice;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         forceOffline.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent("com.zoe.broadcastbestpractice.FORCE_OFFLINE ");
+                Intent intent = new Intent("com.zoe.broadcastbestpractice.FORCE_OFFLINE");
                 sendBroadcast(intent);
             }
         });
